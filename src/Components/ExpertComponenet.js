25e469e7c878be761e27afc1599bc92994e0d369
Bhaskar Maity <maitybhaskar2015@gmail.com>
2021-03-27 11:56:19 +0530

    expert component add

--- src/Components/ExpertComponenet.js
+++ src/Components/ExpertComponenet.js
@@ -1,7 +1,10 @@
 import { Avatar } from "@material-ui/core";
 import React from "react";
 import "./ExpertComponenet.css";
-
+import FavoriteBorderIcon from "@material-ui/icons/FavoriteBorder";
+import TimerIcon from "@material-ui/icons/Timer";
+import LocationOnIcon from "@material-ui/icons/LocationOn";
+import TitleIcon from "@material-ui/icons/Title";
 export default function ExpertComponenet() {
    return (
       <div className="wrapExpert">
@@ -22,7 +25,41 @@ export default function ExpertComponenet() {
                </p>
             </div>
 
-            <div className="second"></div>
+            <div className="second">
+               <div className="heading">
+                  <div className="name">
+                     <h1>Daavid Melvik</h1>
+                     <h5> Founder @ The Milenials</h5>
+                  </div>
+                  <div className="view-profile">
+                     <FavoriteBorderIcon />
+                     <button>View Profile</button>
+                  </div>
+               </div>
+               <div className="offer">
+                  <h5>
+                     <TimerIcon /> 02 Free lessons
+                  </h5>
+                  <h5>
+                     <LocationOnIcon />
+                     Tampa ,Usa
+                  </h5>
+                  <h5>
+                     <TitleIcon />
+                     Greek , Eng, French
+                  </h5>
+               </div>
+               <div className="para">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Necessitatibus dignissimos dolorem alias obcaecati consectetur
+                  consequatur, repellat ab architecto vero sapiente.
+               </div>
+               <div className="specification">
+                  <button className="button">Marketing</button>
+                  <button className="button">Marketing</button>
+                  <button className="button">Marketing</button>
+               </div>
+            </div>
          </div>
       </div>
    );
