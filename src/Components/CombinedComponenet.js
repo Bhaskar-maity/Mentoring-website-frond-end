1a69d936c7b8231081b73c4a686821c856ea765c
Bhaskar Maity <maitybhaskar2015@gmail.com>
2021-03-28 13:46:53 +0530

    footer add

--- src/Components/CombinedComponenet.js
+++ src/Components/CombinedComponenet.js
@@ -2,11 +2,20 @@ import React from "react";
 import RenderFilter from "./Filtercomponent";
 import ExpertComponenet from "./ExpertComponenet";
 import "./CombinedComponenet.css";
+import GitHubIcon from "@material-ui/icons/GitHub";
 export default function CombinedComponenet() {
    return (
-      <div className="whole">
-         <RenderFilter />
-         <ExpertComponenet />
+      <div>
+         <div className="whole">
+            <RenderFilter />
+            <ExpertComponenet />
+         </div>
+         <div className="footer">
+            Developed by 💖Bhaskar maity
+            <a target="_blank" href="https://github.com/Bhaskar-maity">
+               <GitHubIcon />
+            </a>
+         </div>
       </div>
    );
 }
