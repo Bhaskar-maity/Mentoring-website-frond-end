d97f4831e659bd6bbc5f504c4e6f23aa6795e38f
Bhaskar Maity <maitybhaskar2015@gmail.com>
2021-03-28 12:28:43 +0530

    mapping expert json data successful

--- src/Components/ExpertComponenet.js
+++ src/Components/ExpertComponenet.js
@@ -5,6 +5,8 @@ import FavoriteBorderIcon from "@material-ui/icons/FavoriteBorder";
 import TimerIcon from "@material-ui/icons/Timer";
 import LocationOnIcon from "@material-ui/icons/LocationOn";
 import TitleIcon from "@material-ui/icons/Title";
+import Expertdata from "../Expertdata.json";
+
 export default function ExpertComponenet() {
    return (
       <div className="wrapExpert">
@@ -20,57 +22,50 @@ export default function ExpertComponenet() {
             </div>
          </div>
 
-         <div className="box">
-            <div className="first">
-               <Avatar
-                  className="profile-img"
-                  src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
-               />
-               <h5>
-                  Price: <span>FREE</span>
-               </h5>
+         {Expertdata.experts.map((expert) => {
+            return (
+               <div className="box">
+                  <div className="first">
+                     <Avatar className="profile-img" src={expert.profile_img} />
+                     <h5>
+                        Price: <span>FREE</span>
+                     </h5>
 
-               <p>
-                  <span class="fa fa-star checked"></span> 4.9 (120)
-               </p>
-            </div>
+                     <p>
+                        <span class="fa fa-star checked"></span> 4.9 (120)
+                     </p>
+                  </div>
 
-            <div className="second">
-               <div className="heading">
-                  <div className="name">
-                     <h1>Daavid Melvik</h1>
-                     <h5> Founder @ The Milenials</h5>
+                  <div className="second">
+                     <div className="heading">
+                        <div className="name">
+                           <h1>{expert.name}</h1>
+                           <h5> Founder {expert.company}</h5>
+                        </div>
+                        <div className="view-profile">
+                           <FavoriteBorderIcon style={{ color: "coral" }} />
+                           <button>View Profile</button>
+                        </div>
+                     </div>
+                     <div className="offer">
+                        {expert.offer_details.map((offer) => {
+                           return (
+                              <h5>
+                                 <TimerIcon /> {offer}
+                              </h5>
+                           );
+                        })}
+                     </div>
+                     <div className="para">{expert.para}</div>
+                     <div className="specification">
+                        {expert.specification.map((spec) => {
+                           return <button className="button">{spec}</button>;
+                        })}
+                     </div>
                   </div>
-                  <div className="view-profile">
-                     <FavoriteBorderIcon style={{ color: "coral" }} />
-                     <button>View Profile</button>
-                  </div>
                </div>
-               <div className="offer">
-                  <h5>
-                     <TimerIcon /> 02 Free lessons
-                  </h5>
-                  <h5>
-                     <LocationOnIcon />
-                     Tampa ,Usa
-                  </h5>
-                  <h5>
-                     <TitleIcon />
-                     Greek , Eng, French
-                  </h5>
-               </div>
-               <div className="para">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Necessitatibus dignissimos dolorem alias obcaecati consectetur
-                  consequatur, repellat ab architecto vero sapiente.
-               </div>
-               <div className="specification">
-                  <button className="button">Marketing</button>
-                  <button className="button">Marketing</button>
-                  <button className="button">Marketing</button>
-               </div>
-            </div>
-         </div>
+            );
+         })}
       </div>
    );
 }
